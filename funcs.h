403bd3135e6c6d3a62fa1619e7aade5fc478b7e1
juliannea <112399053+juliannea@users.noreply.github.com>
2023-05-03 16:46:35 -0400

    Update funcs.h

--- funcs.h
+++ funcs.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <string>
 
-void invert_image(std::string file);
-void invert_half_image(std::string file);
-void box_image(std::string file);
+void invert_image_colors(std::string file); //changed to fit clearer name 
+void invert_half_image_color(std::string file); //changed to fit clearer name 
+void insert_box_to_image(std::string file); //changed to fit clearer name
 void frame_image(std::string file);
-void scale_image(std::string file);
+void increase_scale_image(std::string file); //changed to fit clearer name 
 void pixelate_image(std::string file);
