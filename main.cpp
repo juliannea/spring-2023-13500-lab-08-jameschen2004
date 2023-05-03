77bae93e0d2dc6b0b18ddad1448cffe9e066b05f
juliannea <112399053+juliannea@users.noreply.github.com>
2023-05-03 16:41:25 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,13 @@
 int main(){
 
   std::cout << "All images read the image found in inImage.pgm \n"; 
-  invert_image("inImage.pgm");
-  invert_half_image("inImage.pgm");
-  box_image("inImage.pgm");
+  invert_image_colors("inImage.pgm");
+  invert_half_image_color("inImage.pgm");
+  insert_box_to_image("inImage.pgm");
   frame_image("inImage.pgm");
-  scale_image("inImage.pgm");
+  increase_scale_image("inImage.pgm");
   pixelate_image("inImage.pgm");
 
   return 0;
 }
+
